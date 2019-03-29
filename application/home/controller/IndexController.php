5fef44135080bc35171115dd34c8d985d8a22734
huyang0826 <860107276@qq.com>
2019-03-29 14:10:23 +0800

    1

--- application/home/controller/IndexController.php
+++ application/home/controller/IndexController.php
@@ -7,7 +7,11 @@ use think\Session;
 class IndexController extends Controller
 {
 	public function index(){
-       
+       	$banner = Db::name('banner')->where('status',1)->order('sort asc')->select();
+       	$this->assign('banner',$banner);
+
+       	$type = Db::name('type')->where('type',1)->where('status',1)->order('sort asc')->select();
+       	$this->assign('type',$type);
         return view();
     }
 
